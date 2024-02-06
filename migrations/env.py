47f9360d2ae2895940993928f4391d8812ab42af
Maru <osmanov.m.i.20@gmail.com>
2024-02-06 17:38:20 +0300

    import fix

--- migrations/env.py
+++ migrations/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from config import DB_HOST, DB_NAME, DB_PASS, DB_PORT, DB_USER
-from models.models import Base
+from database.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
