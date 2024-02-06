22bb57bfba2a78f314627e0b5b48b74704a38437
Maru <osmanov.m.i.20@gmail.com>
2024-02-06 18:53:41 +0300

    Fixed all requests

--- routes/cafe.py
+++ routes/cafe.py
@@ -42,7 +42,7 @@ async def update_cafe(id: int, cafe: CafeUpdate):
             for attr, value in cafe.dict(exclude_unset=True).items():
                 setattr(db_cafe, attr, value)
             await session.commit()
-            await session.refresh(db_cafe)
+        await session.refresh(db_cafe)
     return db_cafe
 
 
@@ -53,6 +53,6 @@ async def delete_cafe(id: int):
             db_cafe = await session.get(CafeModel, id)
             if db_cafe is None:
                 raise HTTPException(status_code=404, detail="Cafe not found")
-            session.delete(db_cafe)
-            await session.commit()
+            await session.delete(db_cafe)
+        await session.commit()
     return {"message": "Cafe deleted successfully"}
