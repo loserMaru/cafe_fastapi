871feb96318b8ef6278b5b02310ab5d2358d3087
Maru <osmanov.m.i.20@gmail.com>
2024-02-07 00:12:02 +0300

    User route. Description and name for Swagger UI

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
-from routes import cafe
+from routes import cafe, user
 
 app = FastAPI(
-    title='Cafe API documentation'
+    title='Cafe API documentation',
+    description='Cafe API',
+    name='Cafe API',
 )
 
-app.include_router(cafe.router, prefix="/api")
+app.include_router(cafe.router, prefix="/api/cafes")
+app.include_router(user.router, prefix="/api/users")
