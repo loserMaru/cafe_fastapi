fcfaac6e97f958a33ae1b283e4110a1afaa24bdf
Maru <osmanov.m.i.20@gmail.com>
2024-02-06 17:36:24 +0300

    Cafe API route

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 from fastapi import FastAPI
+from routes import cafe
 
 app = FastAPI(
     title='Cafe API documentation'
 )
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(cafe.router, prefix="/api")
