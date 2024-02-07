37f929459ca2c6b42906bf8d7e07b4838690b1d5
Maru <osmanov.m.i.20@gmail.com>
2024-02-07 10:10:16 +0300

    Async session

--- database/db.py
+++ database/db.py
@@ -22,8 +22,4 @@ DATABASE_URL = "mysql+aiomysql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAM
 # Создание асинхронного движка SQLAlchemy
 engine = create_async_engine(DATABASE_URL, echo=True, future=True, pool_size=20, max_overflow=0)
 
-async_session = sessionmaker(
-    bind=engine,
-    class_=AsyncSession,
-    expire_on_commit=False,
-)
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
