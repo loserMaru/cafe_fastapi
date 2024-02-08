1f1f664bc1e9d8f0d03736643d804b364539db32
Maru <osmanov.m.i.20@gmail.com>
2024-02-08 15:22:25 +0300

    Uvicorn auto reload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from routes import cafe, user
 
@@ -9,3 +10,6 @@ app = FastAPI(
 
 app.include_router(cafe.router, prefix="/api/cafes")
 app.include_router(user.router, prefix="/api/users")
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
