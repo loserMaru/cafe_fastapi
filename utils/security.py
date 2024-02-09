78b7e40bf538f30b976da36c4b0c06b6bbeb9b39
Maru <osmanov.m.i.20@gmail.com>
2024-02-09 20:38:43 +0300

    Function for jwt security

--- utils/security.py
+++ utils/security.py
@@ -1,4 +1,15 @@
+import os
+from datetime import datetime, timedelta
+
 import bcrypt
+import jwt
+from passlib.context import CryptContext
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ACCESS_TOKEN_EXPIRE_MINUTES = float(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 def hash_password(password: str) -> str:
@@ -18,3 +29,15 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     """
     # Проверка соответствия хэшированного пароля и предоставленного пароля
     return bcrypt.checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
+
+
+def verify_password_pwd(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
